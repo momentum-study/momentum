e0b7444517784ec8b409bf352cf37f89cd62d96e
leightonmascord <leightonmascord@users.noreply.github.com>
2026-08-18 18:29:14 +1000

    Updates

--- assets/subMonths-55_9rxMc.js
+++
@@ -1,1 +0,0 @@
-import{a5 as c,a3 as o}from"./index-FJleJSI7.js";function f(e,a,n){const t=c(e,n==null?void 0:n.in);if(isNaN(a))return o(e,NaN);if(!a)return t;const s=t.getDate(),r=o(e,t.getTime());r.setMonth(t.getMonth()+a+1,0);const u=r.getDate();return s>=u?r:(t.setFullYear(r.getFullYear(),r.getMonth(),s),t)}function M(e,a,n){return f(e,-1,n)}export{f as a,M as s};
